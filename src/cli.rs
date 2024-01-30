485d7f2d1ce43c1164c7dd01edd9684b13c80d3a
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-01-30 16:53:24 -0300

    initial commit

---
+++ src/cli.rs
@@ -0,0 +1,95 @@
+use std::{net::{Ipv4Addr, Ipv6Addr}, path::PathBuf, str::FromStr};
+
+use clap::Parser;
+use trust_dns_resolver::config::{LookupIpStrategy, ResolverConfig, ResolverOpts};
+
+
+#[derive(Debug, Clone, Parser)]
+#[command(version, author)]
+pub struct Args {
+    /// verbosity
+    #[arg(short, action = clap::ArgAction::Count)]
+    pub verbosity: u8,
+
+    /// interval between ping attempts in seconds
+    #[arg(short, long, default_value="15")]
+    pub interval: usize,
+
+    /// output directory for logs
+    #[arg(short = 'o', long, value_parser=parse_log_file_dir)]
+    pub out_dir: Option<PathBuf>,
+
+    /// hostname used for pinging
+    ///
+    /// must be either a valid URL, like "https://youtube.com" or a domain name, like "youtube.com"
+    #[arg(default_value="google.com", value_parser=parse_address)]
+    pub hostname: (Ipv4Addr, Ipv6Addr)
+}
+
+fn parse_log_file_dir(val: &str) -> Result<PathBuf, &'static str> {
+    let path = PathBuf::from(val);
+    if !path.exists() {
+        return Err("Given path does not exist");
+    }
+    if !path.is_dir() {
+        return Err("Giver path is not a directory");
+    }
+    match path.canonicalize() {
+        Ok(o) => return Ok(o),
+        Err(_) => return Err("Could not make path absolute"),
+    }
+}
+
+fn parse_address(val: &str) -> Result<(Ipv4Addr, Ipv6Addr), &'static str> {
+    let url = url::Url::from_str(val);
+
+    let resolver = trust_dns_resolver::Resolver::new(
+        ResolverConfig::cloudflare(),
+        {
+            let mut opts = ResolverOpts::default();
+            opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
+            opts
+        }
+    ).unwrap();
+
+    if let Ok(url) = url {
+        let hostname = match url.host().ok_or("The provided URL does not have a domain")? {
+            url::Host::Domain(d) => d,
+            _ => return Err("The provided URL must have a domain, not an IP address")
+        };
+
+        let lookup = resolver.lookup_ip(hostname);
+
+        match lookup {
+            Ok(o) => {
+                let v4: Option<Ipv4Addr> = o.as_lookup().record_iter().find_map(|r| Some(r.data()?.as_a()?.0));
+                let v6: Option<Ipv6Addr> = o.as_lookup().record_iter().find_map(|r| Some(r.data()?.as_aaaa()?.0));
+
+                match (v4, v6) {
+                    (None, None) => return Err("The provided domain is invalid"),
+                    (None, Some(_)) => return Err("The provided domain does not support IPv4"),
+                    (Some(_), None) => return Err("The provided domain does not support IPv6"),
+                    (Some(v4), Some(v6)) => return Ok((v4, v6)),
+                }
+            },
+            Err(_e) => return Err("There was an error while trying to resolve the hostname"),
+        }
+    } else {
+        let lookup = resolver.lookup_ip(val);
+
+        match lookup {
+            Ok(o) => {
+                let v4: Option<Ipv4Addr> = o.as_lookup().record_iter().find_map(|r| Some(r.data()?.as_a()?.0));
+                let v6: Option<Ipv6Addr> = o.as_lookup().record_iter().find_map(|r| Some(r.data()?.as_aaaa()?.0));
+
+                match (v4, v6) {
+                    (None, None) => return Err("The provided domain is invalid"),
+                    (None, Some(_)) => return Err("The provided domain does not support IPv4"),
+                    (Some(_), None) => return Err("The provided domain does not support IPv6"),
+                    (Some(v4), Some(v6)) => return Ok((v4, v6)),
+                }
+            },
+            Err(_e) => return Err("There was an error while trying to resolve the hostname"),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,175 @@
+use std::{io::{stdin, IsTerminal, Read, Write}, net::IpAddr, sync::{atomic::AtomicBool, Arc}, time::Duration};
+
+use clap::Parser;
+use cli::Args;
+use flexi_logger::{style, Age, Cleanup, Criterion, DeferredNow, FileSpec, LogSpecification, Naming};
+use log::{debug, error, info, trace, Record};
+
+mod cli;
+
+fn formatter_stderr(write: &mut dyn Write, now: &mut DeferredNow, record: &Record) -> std::io::Result<()>{
+    write!(
+        write,
+        "[{} {} {}] {}",
+        now.now(),
+        record.target(),
+        style(record.level()).paint(record.level().to_string()),
+        record.args()
+    )
+}
+
+fn formatter_file(write: &mut dyn Write, now: &mut DeferredNow, record: &Record) -> std::io::Result<()>{
+    write!(
+        write,
+        "[{} {} {}] {}",
+        now.now(),
+        record.target(),
+        record.level(),
+        record.args()
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let args = tokio::task::spawn_blocking(|| { Args::parse() }).await.unwrap();
+
+    let level = match args.verbosity {
+        0 => LogSpecification::info(),
+        1 => LogSpecification::debug(),
+        _ => LogSpecification::trace()
+    };
+
+    let logger = flexi_logger::Logger::with(level)
+        .duplicate_to_stderr(flexi_logger::Duplicate::All)
+        .print_message()
+        .rotate(Criterion::Age(Age::Day), Naming::Numbers, Cleanup::KeepCompressedFiles(30))
+        .format_for_stderr(formatter_stderr)
+        .format_for_files(formatter_file);
+
+    if let Some(dir) = args.out_dir {
+        logger.log_to_file(
+            FileSpec::default()
+                .directory(dir)
+        ).start().unwrap();
+    } else {
+        logger.start().unwrap();
+    }
+
+    info!("logging started");
+    info!("pinging {} for IPv4", args.hostname.0);
+    info!("pinging {} for IPv6", args.hostname.1);
+
+    let (tx, mut rx) = tokio::sync::watch::channel(false);
+
+    let task = tokio::spawn({
+        async move {
+                tokio::signal::ctrl_c().await.unwrap();
+                trace!("received CTRL+C");
+                tx.send(true).unwrap();
+                trace!("sent cancellation signal");
+            }
+
+            // loop {
+            //     let mut key = [0u8];
+            //     std::io::stdin().read(&mut key).unwrap();
+            // }
+        }
+    );
+
+    let mut v4_errors: usize = 0;
+    let mut v4_successes: usize = 0;
+    let mut v4_error_active = false;
+
+    let mut v6_errors: usize = 0;
+    let mut v6_successes: usize = 0;
+    let mut v6_error_active = false;
+
+    let mut interval = tokio::time::interval(Duration::from_secs(args.interval as u64));
+
+    let v4_ip = IpAddr::V4(args.hostname.0);
+    let v6_ip = IpAddr::V6(args.hostname.1);
+
+    loop {
+        let payload: [u8; 256] = core::array::from_fn(|i| i as u8);
+
+        let ipv4_ping = surge_ping::ping(v4_ip, &payload);
+        let ipv6_ping = surge_ping::ping(v6_ip, &payload);
+
+        let (v4_res, v6_res) = tokio::join!(
+            ipv4_ping,
+            ipv6_ping
+        );
+
+        match v4_res {
+            Ok(o) => {
+                trace!("{:?}", o.0);
+                v4_errors = 0;
+                if v4_successes % 10 == 0 {
+                    debug!("{} responded in {}ms", v4_ip, o.1.as_millis());
+                }
+                v4_successes += 1;
+            },
+            Err(e) => {
+                debug!("v4 failed: {e:?}");
+                v4_successes = 0;
+                v4_errors += 1;
+            },
+        }
+
+        match v6_res {
+            Ok(o) => {
+                trace!("{:?}", o.0);
+                v6_errors = 0;
+                if v6_successes % 10 == 0 {
+                    debug!("{} responded in {}ms", v6_ip, o.1.as_millis());
+                }
+                v6_successes += 1;
+            },
+            Err(e) => {
+                debug!("v6 failed: {e:?}");
+                v6_successes = 0;
+                v6_errors += 1;
+            },
+        }
+
+        let v4_down = v4_errors >= 3;
+        let v6_down = v6_errors >= 3;
+
+        match (v4_down, v6_down) {
+            (true, true) => {
+                if !(v4_error_active && v6_error_active) {
+                    error!("network is down!");
+                }
+                (v4_error_active, v6_error_active) = (true, true);
+            },
+            (true, false) => {
+                if !v4_error_active {
+                    error!("IPv4 is down!");
+                }
+                (v4_error_active, v6_error_active) = (true, false);
+            },
+            (false, true) => {
+                if !v6_error_active {
+                    error!("IPv6 is down!");
+                }
+                (v4_error_active, v6_error_active) = (false, true);
+            },
+            (false, false) => {
+                if v4_error_active && v6_error_active {
+                    info!("network is back online");
+                } else if v4_error_active {
+                    info!("IPv4 is back online")
+                } else if v6_error_active {
+                    info!("IPv6 is back online")
+                }
+                (v4_error_active, v6_error_active) = (false, false);
+            },
+        };
+
+        tokio::select! {
+            _ = rx.changed() => { break }
+            _ = interval.tick() => (),
+        }
+    }
+    info!("logging stopped");
+}
